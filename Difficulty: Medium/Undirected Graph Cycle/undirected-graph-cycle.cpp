8abfa5844308c36bea33a1e54351f664fd22377a
ankita31jena <154870628+ankita31jena@users.noreply.github.com>
2026-06-30 17:47:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
+
+    bool dfs(vector<int> adjLs[], vector<int>& vis, int node, int parent) {
+        vis[node] = 1;
+
+        for (auto it : adjLs[node]) {
+            if (vis[it] == 0) {
+                if (dfs(adjLs, vis, it, node))
+                    return true;
+            }
+            else if (it != parent) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     bool isCycle(int V, vector<vector<int>>& edges) {
 
-        vector<vector<int>> adj(V);
+        vector<int> adjLs[V];
 
-        // Build adjacency list
         for (auto &e : edges) {
             int u = e[0];
             int v = e[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+
+            adjLs[u].push_back(v);
+            adjLs[v].push_back(u);
         }
 
-        vector<int> vis(V,0);
+        vector<int> vis(V, 0);
 
-        for(int i=0;i<V;i++){
-            if(!vis[i]){
-                queue<pair<int,int>> q;
-                q.push({i,-1});
-                vis[i]=1;
-
-                while(!q.empty()){
-                    int node=q.front().first;
-                    int parent=q.front().second;
-                    q.pop();
-
-                    for(auto it:adj[node]){
-                        if(!vis[it]){
-                            vis[it]=1;
-                            q.push({it,node});
-                        }
-                        else if(it!=parent){
-                            return true;
-                        }
-                    }
-                }
+        for (int i = 0; i < V; i++) {
+            if (vis[i] == 0) {
+                if (dfs(adjLs, vis, i, -1))
+                    return true;
             }
         }
 
