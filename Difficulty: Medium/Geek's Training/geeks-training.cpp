96ebf8202e5e2c045a4999fd6e22ac6664cd227a
ankita31jena <154870628+ankita31jena@users.noreply.github.com>
2026-08-15 09:10:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Geek's Training/geeks-training.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    int maximumPoints(vector<vector<int>>& mat) {
+
+        int n = mat.size();
+
+        vector<int> prev(3);
+
+        // Day 0
+        prev[0] = mat[0][0];
+        prev[1] = mat[0][1];
+        prev[2] = mat[0][2];
+
+        for (int day = 1; day < n; day++) {
+
+            vector<int> curr(3);
+
+            // Choose activity 0
+            curr[0] = mat[day][0] + max(prev[1], prev[2]);
+
+            // Choose activity 1
+            curr[1] = mat[day][1] + max(prev[0], prev[2]);
+
+            // Choose activity 2
+            curr[2] = mat[day][2] + max(prev[0], prev[1]);
+
+            prev = curr;
+        }
+
+        return max({prev[0], prev[1], prev[2]});
+    }
+};
